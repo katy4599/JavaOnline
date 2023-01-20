c49e30e59e10fba0b2db359b02681b90eb5a51e8
Katy Millard <katy4599@yahoo.com>
2023-01-19 18:57:51 -0600

    House object

---
+++ JavaOnline/src/com/fundamentals/House.java
@@ -0,0 +1,10 @@
+package com.fundamentals;
+
+public class House {
+
+    String foundation;
+    String roofType;
+    String doorCover;
+    int windowSize;
+    public void doorOpenClose() { }
+}
---
+++ JavaOnline/src/com/fundamentals/Main.java
@@ -0,0 +1,14 @@
+package com.fundamentals;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("This is my first sentence");
+        System.out.println("I am new to Java");
+        System.out.println("This Java course is cool");
+        System.out.println("I am learning stuff every day");
+
+        House myHouse = new House();
+        
+    }
+}
